79b5b74760919f050721eba7c00b814493e7bd4e
Andrew Gardner <vardantexcode@gmail.com>
2024-08-27 19:25:32 -0400

    Implemented Home & Deal Page

---
+++ shopping-store/src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DealsComponent } from './components/deals/deals.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [DealsComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
