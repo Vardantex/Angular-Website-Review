62e85ad407f025274f9d2d490fe61ea9fe88f123
Andrew Gardner <vardantexcode@gmail.com>
2024-08-27 21:11:08 -0400

    Product Card Component Added

--- shopping-store/src/app/pages/home/components/products/products.component.ts
+++ shopping-store/src/app/pages/home/components/products/products.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { PRODUCTS } from '../../../../data/products.data';
+import { Product, PRODUCTS } from '../../../../data/products.data';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
   products = PRODUCTS;
+
+
+onAdd(product: Product) {
+  
+}
+
 }
